8c0debc49da452998aa4c01c09ec64d2ee435684
Martin-Prusa <martin.prusa@educanet.cz>
2021-09-03 18:52:46 +0200

    init-commit

---
+++ src/cz/martin/Main.java
@@ -0,0 +1,32 @@
+package cz.martin;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+        //float number = sc.nextFloat();
+        float n;
+        float sum = 0;
+        System.out.println("Zadej číslo:");
+
+        while(true) {
+            try {
+                n = Float.parseFloat(sc.nextLine());
+                break;
+            } catch (Exception e) {
+                System.out.println("Toto není číslo. Zkus to znovu.");
+            }
+        }
+
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("1/"+i+" => "+ 1f/i);
+            sum+=1f/i;
+        }
+
+        System.out.println("Celkem => "+sum);
+    }
+}
